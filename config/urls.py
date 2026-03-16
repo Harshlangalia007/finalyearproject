b9dfd154dbfc0e2073a596e4d01085db917fe64e
Harsh Langalia <harshlangalia@proton.me>
2026-03-16 18:17:51 +0530

    Landing page and other changes

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import landing_page
+
 urlpatterns = [
+    path("", landing_page, name="landing_page"),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("users/", include("apps.users.urls")),
